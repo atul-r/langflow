1acf9c82052c3a108eca2012881d2bfe9328b978
atulr <atul.ram@hitachivantara.com>
2023-11-30 17:40:32 +0530

    fixed issue with logout redirect

--- src/frontend/src/pages/loginPage/index.tsx
+++ src/frontend/src/pages/loginPage/index.tsx
@@ -68,92 +68,93 @@ export default function LoginPage(): JSX.Element {
 
   if(isOidcLogin) {
     if(!cookies.get("id_tkn_lflw")) {
-	window.location.replace("/api/v1/login")
+      window.location.replace("/api/v1/login")
     }
     return <></>;
   } else {
-  return (
-    <Form.Root
-      onSubmit={(event) => {
-        if (password === "") {
+    return (
+      <Form.Root
+        onSubmit={(event) => {
+          if (password === "") {
+            event.preventDefault();
+            return;
+          }
+          signIn();
+          const data = Object.fromEntries(new FormData(event.currentTarget));
           event.preventDefault();
-          return;
-        }
-        signIn();
-        const data = Object.fromEntries(new FormData(event.currentTarget));
-        event.preventDefault();
-      }}
-      className="h-full w-full"
-    >
-      <div className="flex h-full w-full flex-col items-center justify-center bg-muted">
-        <div className="flex w-72 flex-col items-center justify-center gap-2">
-          <span className="mb-4 text-5xl">⛓️</span>
-          <span className="mb-6 text-2xl font-semibold text-primary">
-            Sign in to Langflow
-          </span>
-          <div className="mb-3 w-full">
-            <Form.Field name="username">
-              <Form.Label className="data-[invalid]:label-invalid">
-                Username <span className="font-medium text-destructive">*</span>
-              </Form.Label>
+        }}
+        className="h-full w-full"
+      >
+        <div className="flex h-full w-full flex-col items-center justify-center bg-muted">
+          <div className="flex w-72 flex-col items-center justify-center gap-2">
+            <span className="mb-4 text-5xl">⛓️</span>
+            <span className="mb-6 text-2xl font-semibold text-primary">
+              Sign in to Langflow
+            </span>
+            <div className="mb-3 w-full">
+              <Form.Field name="username">
+                <Form.Label className="data-[invalid]:label-invalid">
+                  Username <span className="font-medium text-destructive">*</span>
+                </Form.Label>
 
-              <Form.Control asChild>
-                <Input
-                  type="username"
-                  onChange={({ target: { value } }) => {
-                    handleInput({ target: { name: "username", value } });
+                <Form.Control asChild>
+                  <Input
+                    type="username"
+                    onChange={({ target: { value } }) => {
+                      handleInput({ target: { name: "username", value } });
+                    }}
+                    value={username}
+                    className="w-full"
+                    required
+                    placeholder="Username"
+                  />
+                </Form.Control>
+
+                <Form.Message match="valueMissing" className="field-invalid">
+                  Please enter your username
+                </Form.Message>
+              </Form.Field>
+            </div>
+            <div className="mb-3 w-full">
+              <Form.Field name="password">
+                <Form.Label className="data-[invalid]:label-invalid">
+                  Password <span className="font-medium text-destructive">*</span>
+                </Form.Label>
+
+                <InputComponent
+                  onChange={(value) => {
+                    handleInput({ target: { name: "password", value } });
                   }}
-                  value={username}
-                  className="w-full"
+                  value={password}
+                  isForm
+                  password={true}
                   required
-                  placeholder="Username"
+                  placeholder="Password"
+                  className="w-full"
                 />
-              </Form.Control>
 
-              <Form.Message match="valueMissing" className="field-invalid">
-                Please enter your username
-              </Form.Message>
-            </Form.Field>
-          </div>
-          <div className="mb-3 w-full">
-            <Form.Field name="password">
-              <Form.Label className="data-[invalid]:label-invalid">
-                Password <span className="font-medium text-destructive">*</span>
-              </Form.Label>
-
-              <InputComponent
-                onChange={(value) => {
-                  handleInput({ target: { name: "password", value } });
-                }}
-                value={password}
-                isForm
-                password={true}
-                required
-                placeholder="Password"
-                className="w-full"
-              />
-
-              <Form.Message className="field-invalid" match="valueMissing">
-                Please enter your password
-              </Form.Message>
-            </Form.Field>
-          </div>
-          <div className="w-full">
-            <Form.Submit asChild>
-              <Button className="mr-3 mt-6 w-full" type="submit">
-                Sign in
-              </Button>
-            </Form.Submit>
-          </div>
-          <div className="w-full">
-            <Link to="/signup">
-              <Button className="w-full" variant="outline" type="button">
-                Don't have an account?&nbsp;<b>Sign Up</b>
-              </Button>
-            </Link>
+                <Form.Message className="field-invalid" match="valueMissing">
+                  Please enter your password
+                </Form.Message>
+              </Form.Field>
+            </div>
+            <div className="w-full">
+              <Form.Submit asChild>
+                <Button className="mr-3 mt-6 w-full" type="submit">
+                  Sign in
+                </Button>
+              </Form.Submit>
+            </div>
+            <div className="w-full">
+              <Link to="/signup">
+                <Button className="w-full" variant="outline" type="button">
+                  Don't have an account?&nbsp;<b>Sign Up</b>
+                </Button>
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
-    </Form.Root>
-  );
-}}
+      </Form.Root>
+    );
+  }
+}
